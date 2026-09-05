512637dfc60d121dfc545e647e8503fada80aa39
me <>
2026-09-05 14:06:00 +0800

    _

--- cli/gate.py
+++ cli/gate.py
@@ -9,7 +9,10 @@ app = typer.Typer()
 
 @app.command()
 def rsync(
-    env_path: str = ".env",
+    env_path: str = typer.Argument(
+        ".env",
+        help="dotenv环境变量路径",
+    ),
 ):
     """同步mysql中grid数据到redis"""
     engine = get_database_engine(env_path)
