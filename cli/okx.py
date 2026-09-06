c725f85e44848c75e133d064fbd2c735cabed112
me <>
2026-09-06 17:39:43 +0800

    feat(okx): add okx grid sync cli tool and related logic
    新增OKX网格策略同步命令行工具，添加对应的数据库同步逻辑，将MySQL中的OKX现货网格数据同步到Redis并更新状态

---
+++ cli/okx.py
@@ -0,0 +1,22 @@
+import asyncio
+
+import typer
+
+from crypto.okx import grid_close, grid_open
+from utils.mysql import get_database_engine
+
+app = typer.Typer()
+
+
+@app.command()
+def sync(
+    env_path: str = typer.Argument(
+        ".env",
+        help="dotenv环境变量路径",
+    ),
+):
+    """同步mysql中grid数据到redis"""
+    engine = get_database_engine(env_path)
+
+    asyncio.run(grid_open(engine))
+    asyncio.run(grid_close(engine))
---
+++ crypto/okx.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Engine
+
+from utils import logger
+from utils.mysql import mysql_to_redis
+
+
+async def grid_open(engine: Engine):
+    query = "select * from okx_spot where ((cost is not null or benefit is not null) and profit is null) and up_status = 0 and order_id is not null and deleted_at is null limit 20;"
+    key_prefix = "okx_grid"
+    table = "okx_spot"
+
+    row_count = await mysql_to_redis(
+        engine,
+        key_prefix,
+        table,
+        query,
+        update_status=1,
+        d_column_names=["order_id", "client_order_id"],
+        pd_dtype={
+            "order_id": str,
+            "fx_order_id": str,
+            "created_at": "datetime64[ns]",
+            "open_at": "datetime64[ns]",
+            "close_at": "datetime64[ns]",
+        },
+    )
+
+    logger.info(f"🧮 okx grid open count:({row_count})")
+
+
+async def grid_close(engine: Engine):
+    query = "select * from okx_spot where profit is not null and up_status in (0,1) and deleted_at is null limit 20;"
+    key_prefix = "okx_grid"
+    table = "okx_spot"
+
+    row_count = await mysql_to_redis(
+        engine,
+        key_prefix,
+        table,
+        query,
+        update_status=2,
+        d_column_names=["order_id", "client_order_id"],
+        pd_dtype={
+            "order_id": str,
+            "fx_order_id": str,
+            "created_at": "datetime64[ns]",
+            "open_at": "datetime64[ns]",
+            "close_at": "datetime64[ns]",
+        },
+    )
+
+    logger.info(f"🧮 okx grid close count:({row_count})")
